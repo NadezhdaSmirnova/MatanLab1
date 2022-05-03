37384323ca95292798396ec2d29bbd1927d216bf
NadezhdaSmirnova <103686294+NadezhdaSmirnova@users.noreply.github.com>
2022-05-03 04:15:24 +0300

    Update code.py

--- code.py
+++ code.py
@@ -11,31 +11,39 @@ def integral(num_of_points, sposob_osnashenia):
   fig = plt.figure(figsize=(16 *2/3, 8*2/3))
   ax = fig.add_subplot()
   plt. plot(a, y(a), color =(0.1, 0.5, 0.9))
-  
   x = [0] * num_of_points
   for i in range (num_of_points):
       x[i] = 1 + i/(num_of_points - 1)
   e = [0] * (num_of_points - 1)
-  dx = 1/num_of_points
-  
+  dx = 1/(num_of_points-1)
   if sposob_osnashenia == 1:
     for i in range (num_of_points - 1):
       e[i] = 1 + (i+1)/(num_of_points - 1)
+    pog = 8 * math.log(4)/(num_of_points-1)
+    so="правое"
   if sposob_osnashenia == 0:
     for i in range (num_of_points - 1):
       e[i] = 1 + (i+0.5)/(num_of_points - 1)
+    pog = 16 * math.log(4)**2/(24 * (num_of_points-1)**2)
+    so="центральное"
   if sposob_osnashenia == -1:
     for i in range (num_of_points - 1):
       e[i] = 1 + i/(num_of_points - 1)
-      
+    pog = 8 * math.log(4)/(num_of_points-1)
+    so="левое"
   sum = 0
   for i in range (num_of_points - 1):
     sum += 4**e[i] * dx
     rect = Rectangle((x[i], 0), dx, y(e[i]), edgecolor = (1, 0, 0.5), fill=False)
     ax.add_patch(rect)
-    
   print("\n")
+  print("Количество точек разбиения:" + str(num_of_points))
+  print("Выбор оснащения:" + so)
+  print("Разность:" + str(12/math.log(4)-sum) + " Погрешность:" + str(pog))
+  # if pog - (12/math.log(4)-sum) >= 0:
+  #   print("ok")
   print("Сумма:" + str(sum))
+
   plt.show()
 
 integral(30, 1)
